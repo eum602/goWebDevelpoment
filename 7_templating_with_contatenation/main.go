45c3d60b1abac9c523f3a527429e2b3a958d21bd
Erick <eum602@gmail.com>
2021-05-02 22:03:42 -0500

    saves template into a *.html file

--- 7_templating_with_contatenation/main.go
+++ 7_templating_with_contatenation/main.go
@@ -1,20 +1,35 @@
 package main
- import "fmt"
 
- func main()  {
-	 name:= "eum602";
-	 tpl:= `
-	 <!DOCTYPE html>
-	 <html lan="en">
-	 <head>
-	 <meta charset="UTF-8">
-	 <title>Hello World!</title>
-	 </head>
-	 <body>
-	 <h1>` + name + `</h1>
-	 </body> 
-	 </html>
-	 `
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
 
-	 fmt.Println(tpl);
- }
+func main() {
+	name := "eum602"
+	str := fmt.Sprint(`
+		<!DOCTYPE html>
+		<html lan="en">
+		<head>
+		<meta charset="UTF-8">
+		<title>Hello World!</title>
+		</head>
+		<body>
+		<h1>` + name + `</h1>
+		</body> 
+		</html>
+		`)
+
+	nf, err := os.Create("index.html")
+
+	if err != nil {
+		log.Fatal("error creating file", err)
+	}
+
+	defer nf.Close()
+
+	io.Copy(nf, strings.NewReader(str))
+}
