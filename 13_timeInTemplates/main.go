4ad485b17ee63ebce9c9eae74ab865207b0623c7
Erick <eum602@gmail.com>
2021-05-08 15:38:04 -0500

    adds an example about how to insert a function time with D-M-Y format

--- 13_timeInTemplates/main.go
+++ 13_timeInTemplates/main.go
@@ -15,10 +15,18 @@ func init() {
 
 func monthDayYear(t time.Time) string {
 	return t.Format("01-02-2006")
+	//01/02 03:04:05 MST 2006 -> january (01 is month) second (02 is the day), 03 hours (03 is hours) 04 minutes (04 are the minutes) 05 seconds (05 are the seconds) of year 2006 (2006 represents the year)
+
+	//for example if I put t.Format("02-01-2006") then if means the day goes first the the month and then the year
+}
+
+func dayMonthYear(t time.Time) string {
+	return t.Format("02-01-2006")
 }
 
 var fm = template.FuncMap{
 	"fdateMDY": monthDayYear,
+	"fdateDMY": dayMonthYear,
 }
 
 func main() {
