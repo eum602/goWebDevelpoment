0a6d825cc1eeb9087d498c3cb6ab409a388e8a7c
Erick <eum602@gmail.com>
2021-05-09 12:59:16 -0500

    adds a DeadLine for each connection before it is closed, also echoes the answer to the client

--- 22_buffioScanner/main.go
+++ 22_buffioScanner/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 var counter = 0
@@ -26,7 +27,12 @@ func main() {
 	}
 }
 
-func handle(conn net.Conn) {
+func handle(conn net.Conn) { //User TELNET to test it
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second)) //every connection will only remain 10 seconds!
+	if err != nil {
+		log.Println(err)
+	}
+
 	counter++
 	cnt := counter
 	fmt.Println("counter ", cnt)
@@ -34,12 +40,11 @@ func handle(conn net.Conn) {
 	for scanner.Scan() { //scanner.Scan returns a bool -> so it scans as long as there are LINES to read.
 		ln := scanner.Text() //give me the line
 		fmt.Println("room ", cnt, ": ", ln)
+		fmt.Fprintf(conn, "I hear from the server %s\n", ln) //echoes the input to the issuer.
 	}
 
 	defer conn.Close()
 
-	//never gets here because the connection is constantly opened, but if I open a connection with telnet and I
-	//suddenly close it then the code reaches here
-	fmt.Println("code got here")
+	fmt.Println("code got here") //the code gets either because the client closes the connection or the SetDeadline reaches the configured time
 
 }
