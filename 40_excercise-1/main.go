31274f5e257e7e7951d3bf6497b022ae32da68e2
Erick <eum602@gmail.com>
2021-05-19 22:03:04 -0500

    adds Handler, into te default mux, by using HandlerFunc

--- 40_excercise-1/main.go
+++ 40_excercise-1/main.go
@@ -6,35 +6,27 @@ import (
 	"text/template"
 )
 
-type home int
-type dog int
-type me int
-
 var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
-func (m home) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func home(w http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(w, "index.gohtml", "some message")
 }
 
-func (m me) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func me(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello from me!!!")
 }
 
-func (m dog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func dog(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello from dog!!!")
 }
 
 func main() {
-	var d dog
-	var m me
-	var h home
-	http.Handle("/", h)
-	http.Handle("/dog/", d)
-	http.Handle("/me/", m)
+	http.Handle("/", http.HandlerFunc(home))
+	http.Handle("/dog/", http.HandlerFunc(dog))
+	http.Handle("/me/", http.HandlerFunc(me))
 	http.ListenAndServe(":8080", nil)
-
 }
