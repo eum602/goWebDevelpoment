764126097933490da3acf2a20439d8900f3c9a5e
Erick <eum602@gmail.com>
2021-05-29 15:02:33 -0500

    serve-files: adds another way to serve static files

--- 46_staticServerFiles/main.go
+++ 46_staticServerFiles/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var tpl *template.Template
+
 func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
+	http.Handle("/vendor/", http.StripPrefix("/vendor", http.FileServer(http.Dir("vendor"))))
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "index.html", nil)
 }
