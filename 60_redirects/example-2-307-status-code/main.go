9c91c7e0645ef99ff7a52ea3895213238ecf2f88
Erick <eum602@gmail.com>
2021-06-05 19:27:08 -0500

    explaisn how to use 307 status code -  adds clarification of 303 (status always translates to GET) and 307 (status remains the same)

--- 60_redirects/example-2-307-status-code/main.go
+++ 60_redirects/example-2-307-status-code/main.go
@@ -27,15 +27,14 @@ func foo(w http.ResponseWriter, req *http.Request) {
 func bar(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Your request method at bar", req.Method)
 	//set the new location where the client can get the resource
-	//w.Header().Set("Location", "/")
-	//w.WriteHeader(http.StatusSeeOther) //redirects with status code 303
-	http.Redirect(w, req, "/", http.StatusSeeOther) //this makes the same like writing header and setting the location
-	//what it does is send the client to "/". The method always translated to 'GET' that is if the method was 'POST' or 'GET' its
-	//becomes 'GET' after redirection.
+	w.Header().Set("Location", "/")
+	w.WriteHeader(http.StatusTemporaryRedirect) //307 makes the METHOD STAY THE SAME during redirection, that is if the method was
+	//POST then the redirection will stay POST. The same for GET
+	//what it does is send the client to "/"
 }
 
 func barred(w http.ResponseWriter, req *http.Request) {
-	//if a user goes to "/barred" then the form will hit "/bar" which in turn will move with status "303 - see other" to "/"
+	//if a user goes to "/barred" then the form will hit "/bar" which in turn will move with status "307" - temporary redirect" to "/"
 	fmt.Println("Your request method at barred:", req.Method)
 	tpl.ExecuteTemplate(w, "index.gohtml", nil)
 }
